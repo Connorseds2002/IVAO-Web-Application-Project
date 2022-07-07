217c2c5247bd58095098894c1c2ef9ad12550aa0
Connor Sedman <connorsedman.vatsim@gmail.com>
2022-07-07 13:31:19 +0100

    API Update

--- ivao-api/src/app.module.ts
+++ ivao-api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { addBookingModule } from './ATC Bookings/AtcBookings.module';
 
 @Module({
-  imports: [addBookingModule],
+  imports: [addBookingModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'admin',
+    password: 'admin123',
+    database: 'bookings',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
